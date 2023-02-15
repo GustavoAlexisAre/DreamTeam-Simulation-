541616d41c117bfce1cbb0efb62332db09b017af
Necrosz <g.alexisarellano@gmail.com>
2023-02-15 10:28:20 -0600

    mas equipos

--- services/futbolApi.service.js
+++ services/futbolApi.service.js
@@ -4,7 +4,7 @@ module.exports = function getFootballFixtures() {
   const options = {
     method: 'GET',
     url: 'https://api-football-v1.p.rapidapi.com/v3/fixtures',
-    params: {league: '2', season: '2022', from: '2023-02-13', to: '2023-02-15'},
+    params: {league: '2', season: '2022', from: '2023-02-13', to: '2023-03-15'},
     headers: {
       'X-RapidAPI-Key': '6f605d22abmshb359d7b0d088614p103d73jsna8a1241fd0a1',
       'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
